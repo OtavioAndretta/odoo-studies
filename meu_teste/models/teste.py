9c7082c3dd87a22140c49b956fdc75abcf35426b
OTAVIO BARBOSA ANDRETTA <96994962+IntelAthom1Ghz@users.noreply.github.com>
2025-08-19 10:17:34 -0300

    first test module

---
+++ meu_teste/models/teste.py
@@ -0,0 +1,16 @@
+from odoo import models, fields, api
+
+class TestePagamentos(models.Models):
+    _name ="meu_teste.pagamento"
+    _description = "Teste de pagamento"
+    
+    name = fields.Char(string='Nome do Pagamento') #label que aparece na interface do Odoo.
+    state = fields.Selection([
+        ('draft','Rascunho'),
+        ('posted','Publicado'),
+        ('cancel','Cancelado') 
+        
+    ], default = 'draft') #odo novo registro começa como "Rascunho". esse campo é usado para controlar o estado do pagamento, semelhante a workflow.
+
+
+    partner_id = fields.Many2One('res.partner', string ='Cliente') #funciona como uma chave estrangeira: cada pagamento pode ter apenas um cliente associado.
