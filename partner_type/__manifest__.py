43c35ccbb16410fd547ab452087ab576955498dc
OTAVIO BARBOSA ANDRETTA <96994962+IntelAthom1Ghz@users.noreply.github.com>
2025-08-20 14:42:16 -0300

    minor changes

--- partner_type/__manifest__.py
+++ partner_type/__manifest__.py
@@ -1,16 +1,16 @@
 {
-    "name":"Partner Expanded",
-    "version":"1.0",
-    "summary":"adiciona seleção de tipo de cliente",
-    "description":"Foi adicionado usando fieldsSelection 3 seções de clientes, regular, VIP e Prospect",
-    "author":"Otávio Andretta",
-    "depends":['base'],
-    "data":[
-        "views/partner_type_views.xml"
-    ],
+    "name": "Partner Expanded",
+    "version": "1.0",
+    "summary": "adiciona seleção de tipo de cliente",
+    "description": "Foi adicionado usando fieldsSelection 3 seções de clientes, regular, VIP e Prospect",
+    "author": "Otávio Andretta",
+    "depends": ['base'],
+    "data": [
+         "views/partner_type_views.xml"
+     ],
     
-    "installable":True,
-    "application":False,
-    "auto_install":False
+    "installable": True,
+    "application": False,
+    "auto_install": False
 }
 
